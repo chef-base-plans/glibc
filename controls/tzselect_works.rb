38712429072e2252d5483a2500941d43368ccc44
Steven Marshall <SMarshall@chef.io>
2020-06-16 14:33:57 +0100

    [glibc] - Change control to use input
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/tzselect_works.rb
+++ controls/tzselect_works.rb
@@ -6,7 +6,7 @@ describe bash("echo '2\n49\n22\n1' >> tz-test.txt") do
   its('exit_status') { should eq 0}
 end
 
-describe bash("/bin/tzselect < tz-test.txt 2>&1 | tail -n1 ") do
+describe bash("#{tzselect} < tz-test.txt 2>&1 | tail -n1 ") do
   its('stdout.strip') { should eq "#? America/Anchorage" }
   its('stderr') { should be_empty}
   its('exit_status') { should eq 0 }
