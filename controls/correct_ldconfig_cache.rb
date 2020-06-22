a3a78baae3b917118fc47fc1aeb05c110350e53c
Steven Marshall <SMarshall@chef.io>
2020-06-22 12:19:38 +0100

    [glibc] various refactors of existing code
    Updated README to new binary format.
    Added inspec.lock to .gitignore.
    Updated the controls to use the control block inspec formatting.
    Updated inspec.yml to latest version.
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/correct_ldconfig_cache.rb
+++ controls/correct_ldconfig_cache.rb
@@ -1,24 +1,43 @@
+title 'Tests to confirm ldconfig cache only contains this package' 
+
 plan_origin = ENV["HAB_ORIGIN"]
 plan_name = input("plan_name", value: "glibc")
 plan_ident = "#{plan_origin}/#{plan_name}"
-ldconfig_path = input("ldconfig_path", value: "/bin/ldconfig")
 
-hab_pkg_path = command("hab pkg path #{plan_ident}")
-describe hab_pkg_path do
-  its("exit_status") { should eq 0 }
-  its("stdout") { should_not be_empty }
-end
+control 'core-plans-check-ld-config' do
+  impact 1.0
+  title 'Ensure ld-config only contains this pacakge'
+  desc '
+  To test the ld-config only contains this package, we first have to locate the ldconfig executable.
+  We then use this executable to print the cache, so that it can be checked.
+  The returned value should contain the libutil.so.1 file that was built with the current package.
+    $ PKG_PATH/bin/ldconfig --print-cache | tail -n +1 | awk '{ print $8 }'
+    PKG_PATH/lib/libutil.so.1
+    ...  
+  '
 
-target_file = File.join(hab_pkg_path.stdout.strip, ldconfig_path)
+  hab_pkg_path = command("hab pkg path #{plan_ident}")
+  describe hab_pkg_path do
+    its("stdout") { should_not be_empty }
+    its("stderr") { should be_empty }
+    its("exit_status") { should eq 0 }
+  end
 
-ldd_search_path_check = command("#{target_file} --print-cache | tail -n +1 | awk '{print $8}'")
-describe ldd_search_path_check do
-  its("exit_status") { should eq 0 }
-  its("stdout") { should_not be_empty }
-  for line in ldd_search_path_check.stdout.split()
-    describe command("echo #{line} | cut -d/ -f1-7") do
-      its("exit_status") { should eq 0 }
-      its("stdout.strip") { should eq "#{hab_pkg_path.stdout.strip}" }
+  ldconfig_binary = File.join(hab_pkg_path.stdout.strip, "/bin/ldconfig")
+
+  ldconfig_check = command("#{ldconfig_binary} --print-cache | tail -n +1 | awk '{ print $8 }'")
+  describe ldconfig_check do
+    its("stdout") { should_not be_empty }
+    its("stderr") { should be_empty}
+    its("exit_status") { should eq 0}
+
+    for line in ldconfig_check.stdout.split()
+      describe command("echo #{line} | cut -d/ -f1-7") do
+        its("stdout.strip") { should eq "#{hab_pkg_path.stdout.strip}" }
+        its("stderr") { should be_empty }
+        its("exit_status") { should eq 0 }
+      end
     end
   end
+
 end
--- controls/correct_ldd_textdomaindir.rb
+++ controls/correct_ldd_textdomaindir.rb
@@ -1,20 +1,35 @@
+Title 'Tests to ensure correct textdomaindir is set'
+
 plan_origin = ENV["HAB_ORIGIN"]
 plan_name = input("plan_name", value: "glibc")
 plan_ident = "#{plan_origin}/#{plan_name}"
-ldd_search_path = input("ldd_search_path", value: "/bin/ldd")
-textdomaindir = input("rtldlist", value: "/share/locale")
 
-hab_pkg_path = command("hab pkg path #{plan_ident}")
-describe hab_pkg_path do
-  its("exit_status") { should eq 0 }
-  its("stdout") { should_not be_empty }
-end
+control 'core-plans-check-textdomaindir' do
+  impact 1.0
+  title 'Ensure correct textdomaindir is set'
+  desc '
+  To test the correct textdomaindir has been set, we must first locate the ldd binary and the path to the locale file in the share directory.
+  We then search through the ldd binary for the TEXTDOMAINDIR.
+  The returned value should equal the /share/locale file.
+    $ grep "TEXTDOMAINDIR" "$PKG_PATH/bin/ldd" | cut -d '=' -f2
+    $PKG_PATH/share/locale
+  '
 
-target_file = File.join(hab_pkg_path.stdout.strip, ldd_search_path)
-expected_file = File.join(hab_pkg_path.stdout.strip, textdomaindir)
+  hab_pkg_path = command("hab pkg path #{plan_ident}")
+  describe hab_pkg_path do
+    its("stdout") { should_not be_empty }
+    its("stderr") { should be_empty}
+    its("exit_status") { should eq 0 }
+  end
 
-ldd_search_path_check = command("grep TEXTDOMAINDIR #{target_file} | cut -d'=' -f2")
-describe ldd_search_path_check do
-  its("exit_status") { should eq 0 }
-  its("stdout.strip") { should eq "#{expected_file}" }
+  ldd_binary = File.join(hab_pkg_path.stdout.strip, "/bin/ldd")
+  expected_file = File.join(hab_pkg_path.stdout.strip, "/share/locale")
+
+  ldd_textdomaindir_check = command("grep TEXTDOMAINDIR #{ldd_binary} | cut -d '=' -f2")
+  describe ldd_textdomaindir_check do
+    its("stdout.strip") { should eq "#{expected_file}" }
+    its("stderr") { should be_empty }
+    its("exit_status") { should eq 0 }
+  end
+
 end
